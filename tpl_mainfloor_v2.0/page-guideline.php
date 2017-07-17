7403beb343b960e9118ba4f12d3d0bb16c5e0782
nakamura enjoy <nakamura@enjoytrust.net>
2017-07-17 14:07:55 +0900

    最新データ

--- tpl_mainfloor_v2.0/page-guideline.php
+++ tpl_mainfloor_v2.0/page-guideline.php
@@ -35,38 +35,26 @@
         <li class="guide-listin">
           <p class="guidelist-index">文章（ベーシック）</p>
           <p class="guidelist-textlead">
-            一番大きな見出しです。話の大きい区切りとして使用してください。
+            一番良く使用するベーシックなテキストです。文中に改行は絶対に含めないでください。
           </p>
         </li>
         <li class="guide-listin">
-          <p class="guidelist-index">大見出し</p>
+          <p class="guidelist-index">文章（カスタム）</p>
           <p class="guidelist-textlead">
-            一番大きな見出しです。話の大きい区切りとして使用してください。
+            見出し、文章などHTML形式で入力ができます。文中の太字、リンクなどを使用する時などに使用してください。
           </p>
         </li>
         <li class="guide-listin">
-          <p class="guidelist-index">大見出し</p>
+          <p class="guidelist-index">備考・注意書きテキスト</p>
           <p class="guidelist-textlead">
-            一番大きな見出しです。話の大きい区切りとして使用してください。
+            本文ではなく小さなテキストになります。注意書きとして、備考や補足など多目的に使用してください。
           </p>
         </li>
-        <li class="guide-listin">
-          <p class="guidelist-index">大見出し</p>
-          <p class="guidelist-textlead">
-            一番大きな見出しです。話の大きい区切りとして使用してください。
-          </p>
-        </li>
-        <li>小見出し</li>
-        <li>リンク</li>
-        <li>文章（ベーシック）<br>メインの文章はこの項目を選択します。PC、スマートフォンでの表示崩れを防ぐ為に改行は使用しません。</li>
-        <li>文章（カスタム）<br>文中に太字、リンクを表現する為の項目です。HTMLタグを用いて文章を作成します。ただし、使用タグは全てクイックタグから選択してください。</li>
-        <li>記事タイトル<br>メインの記事タイトルです。ほぼすべてのページにて表示されますので文字数には注意してください。※推奨文字数20～40文字程度</li>
-        <li>記事ダイジェスト<br>記事の導入文です。「続きを読みたい」と思える魅力的な導入文を書いてみましょう</li>
-        <li>目次</li>
-        <li></li>
       </ul>
 
       <h3 class="guide-index">注意事項</h3>
+        <p>*文章（カスタム）でHTMLを入力するときは必ず、クイックタグより選んでください。手入力の場合は表示崩れを起こします。</p>
+        <p>*文章を改行したい。改行して余白をあけたい場合はかならず新しく文章の入力欄を追加してください。</p>
 
     </div>
   </section>
